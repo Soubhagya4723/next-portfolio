697a1d0535d32de292fa6bf79b5530ade21b33a3
soubhagya <soubhagyakumar373@gmail.com>
2026-03-12 22:14:56 +0530

    change context

--- components/hero.tsx
+++ components/hero.tsx
@@ -33,7 +33,7 @@ export const Hero = () => {
 
           <TextGenerateEffect
             className="text-center text-[40px] md:text-5xl lg:text-6xl"
-            words="Transforming Concepts into Seamless User Experiences"
+            words="Transforming Concepts into Seamless User "
           />
 
           <p className="mb-4 text-center text-sm md:text-lg md:tracking-wider lg:text-2xl">
